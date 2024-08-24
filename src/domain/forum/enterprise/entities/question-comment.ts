2a4add251a9eb07a3eded0a1d0d674a7120d47ac
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-24 10:41:02 -0300

    feat: adds comments entities

---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,49 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+interface QuestionCommentProps {
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class QuestionComment extends Entity<QuestionCommentProps> {
+  public static create(
+    props: Optional<QuestionCommentProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ): QuestionComment {
+    const questionComment = new QuestionComment(
+      { ...props, createdAt: props.createdAt ?? new Date() },
+      id,
+    )
+    return questionComment
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+}
