c5e795607f0a3a3d2119e17a5d38d72630c96f3e
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-27 20:16:29 -0300

    feat: comment on answer

--- src/domain/forum/application/usecases/comment-on-question.spec.ts
+++ src/domain/forum/application/usecases/comment-on-question.spec.ts
@@ -1,37 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
 
-import { makeQuestion } from 'test/factories/make-question'
-import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
-import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { CommentOnQuestionUseCase } from './comment-on-question'
-
-describe('Comment on Question [Use Case]', () => {
-  let questionCommentsRepository: InMemoryQuestionCommentsRepository
-  let questionsRepository: InMemoryQuestionsRepository
-  let sut: CommentOnQuestionUseCase
+describe('Comment on Answer [Use Case]', () => {
+  let answerCommentsRepository: InMemoryAnswerCommentsRepository
+  let answersRepository: InMemoryAnswersRepository
+  let sut: CommentOnAnswerUseCase
 
   beforeEach(() => {
-    questionCommentsRepository = new InMemoryQuestionCommentsRepository()
-    questionsRepository = new InMemoryQuestionsRepository()
-    sut = new CommentOnQuestionUseCase(
-      questionsRepository,
-      questionCommentsRepository,
+    answerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    answersRepository = new InMemoryAnswersRepository()
+    sut = new CommentOnAnswerUseCase(
+      answersRepository,
+      answerCommentsRepository,
     )
   })
 
-  it('should be able to comment on question', async () => {
-    const question = makeQuestion()
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
 
-    await questionsRepository.create(question)
+    await answersRepository.create(answer)
 
     await sut.execute({
-      authorId: question.authorId.toString(),
-      questionId: question.id.toString(),
+      authorId: answer.authorId.toString(),
+      answerId: answer.id.toString(),
       content: 'Comment content',
     })
 
-    expect(questionCommentsRepository.items[0].content).toEqual(
-      'Comment content',
-    )
+    expect(answerCommentsRepository.items[0].content).toEqual('Comment content')
   })
 })
